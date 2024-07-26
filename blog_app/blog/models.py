a9213b127edb2b10a5779274618b766b5c5a00b0
Rick Verbon <R.Verbon@tilburguniversity.edu>
2024-07-26 14:18:52 +0200

    Started with the book

---
+++ blog_app/blog/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.conf import settings
+
+
+# Custom Manager
+class PublishedManager(models.Manager):
+    """ Custom Manager to retrieve only published posts. """
+    def get_queryset(self):
+        """ Overriding the get_queryset method to filter only published posts. """
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+
+# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField( 
+        max_length=2,
+        choices=Status, # type: ignore
+        default=Status.DRAFT
+    )
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
+    class Meta:
+        ordering = ['-publish',]
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ] # type: ignore
+        )
---
+++ blog_app/blog/views.py
@@ -0,0 +1,46 @@
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
+from blog.models import Post
+from blog.forms import EmailPostForm
+
+
+class PostListView(ListView):
+    """ Alternative post list view """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post, 
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(request, 'blog/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id.
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation.
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject=subject, message=message, from_email=None, recipient_list=[cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/share.html', {'post': post, 'form': form, 'sent': sent})
